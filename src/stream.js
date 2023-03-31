487f15511e8ab39ced01f052c19ecd10e714e8f5
Joepolymath <joshuaajagbe96@gmail.com>
2023-03-31 11:10:01 +0100

    initial commit

---
+++ src/stream.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const file = path.join(__dirname, 'data.txt');
+const readableStream = fs.createReadStream(file);
+
+readableStream.on('data', (chunk) => {
+  console.log(`Received ${chunk.length} bytes of data -> ${chunk}`);
+});
+
+readableStream.on('end', () => {
+  console.log('End of stream.');
+});
